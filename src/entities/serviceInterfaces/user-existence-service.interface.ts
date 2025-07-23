dcf896aa8180623a2ebf697d57e6fc8d190e59c7
SOURAV-RK <souravrk25@gmail.com>
2025-07-23 08:40:07 +0530

    added interface for the get user and role by mail functioln

--- src/entities/serviceInterfaces/user-existence-service.interface.ts
+++ src/entities/serviceInterfaces/user-existence-service.interface.ts
@@ -1,3 +1,4 @@
 export interface IUserExistenceService{
     emailExists(email : string) : Promise<boolean>;
+    getUserAndRoleByEmail(email: string): Promise<{ user: any; role: string } | null>;
 }
