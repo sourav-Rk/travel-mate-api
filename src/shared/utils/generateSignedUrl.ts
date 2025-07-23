5f19ab1f4fa45be4c42ff7d96923e94449367b46
SOURAV-RK <souravrk25@gmail.com>
2025-07-23 08:32:41 +0530

    added utility function for signed url

---
+++ src/shared/utils/generateSignedUrl.ts
@@ -0,0 +1,14 @@
+import { cloudinary } from "../../frameworks/cloudinary/cloudinary";
+
+export async function generateSignedUrl(publiceId : string, expiresInSec = 300) : Promise<string> {
+    const timestamp = Math.floor(Date.now() / 1000) + expiresInSec;
+
+    const url = cloudinary.url(publiceId,{
+        type : "authenticated",
+        sign_url : true,
+        secure : true,
+        expires_at :timestamp
+    });
+
+    return url
+}
