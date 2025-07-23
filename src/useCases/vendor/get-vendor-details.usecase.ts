aeea8b73a80f652955e9e1d2e8acc33a42115a83
SOURAV-RK <souravrk25@gmail.com>
2025-07-23 08:29:50 +0530

    added get-vendor details usecase

--- src/useCases/vendor/get-vendor-details.usecase.ts
+++ src/useCases/vendor/get-vendor-details.usecase.ts
@@ -1,30 +1,30 @@
 import { inject, injectable } from "tsyringe";
-import { IGetVendorDetailsUsecase } from "../../entities/useCaseInterfaces/vendor/get-vendor-details.usecase.interface";
+import { IGetVendorDetailsUsecase } from "../../entities/useCaseInterfaces/vendor/get-vendor-details-usecase.interface";
 import { IVendorEntity } from "../../entities/modelsEntity/vendor.entity";
-import { IVendorRepository } from "../../entities/repositoryInterfaces/vendor/vendor-repository.interface";
 import { CustomError } from "../../shared/utils/error/customError";
 import { ERROR_MESSAGE, HTTP_STATUS } from "../../shared/constants";
+import { IVendorRepository } from "../../entities/repositoryInterfaces/vendor/vendor-repository.interface";
 
 @injectable()
 export class GetVendorDetailsUsecase implements IGetVendorDetailsUsecase {
-  constructor(
-    @inject("IVendorRepository")
-    private vendorRepository: IVendorRepository
-  ) {}
+    constructor(
+        @inject('IVendorRepository')
+        private _vendorRepository : IVendorRepository
+    ){}
 
-  async execute(vendorId: any): Promise<Partial<IVendorEntity | null>> {
-    const vendor = await this.vendorRepository.findById(vendorId);
+    async execute(id: any): Promise<IVendorEntity | null> {
+        if(!id) {
+            throw new CustomError(HTTP_STATUS.BAD_REQUEST,"Vendor id is missing");
+        }
 
-    if (!vendor) {
-      throw new CustomError(
-        HTTP_STATUS.NOT_FOUND,
-        ERROR_MESSAGE.USER_NOT_FOUND
-      );
+        const vendor = await this._vendorRepository.findById(id);
+
+        if(!vendor){
+            throw new CustomError(HTTP_STATUS.BAD_REQUEST,ERROR_MESSAGE.USER_NOT_FOUND);
+        }
+
+        const vendorDetails = await this._vendorRepository.getVendorWithAddressAndKyc(id);
+
+        return vendorDetails;
     }
-    return {
-      _id :vendor._id,
-      status : vendor.status,
-      agencyName : vendor.agencyName
-    }
-  }
 }
