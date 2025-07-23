bd522d8913c8d8ae24b26c85991aa6dd8ae884cd
SOURAV-RK <souravrk25@gmail.com>
2025-07-23 08:29:05 +0530

    added signed url usecase

---
+++ src/useCases/common/generateSignedUrl.usecase.ts
@@ -0,0 +1,37 @@
+import { injectable } from "tsyringe";
+import { IGenerateSignedUrlUsecase } from "../../entities/useCaseInterfaces/common/generate-signedurl-usecase.interface";
+import { HTTP_STATUS } from "../../shared/constants";
+import { CustomError } from "../../shared/utils/error/customError";
+import { generateSignedUrl } from "../../shared/utils/generateSignedUrl";
+
+@injectable()
+export class GenerateSignedUrlUsecase implements IGenerateSignedUrlUsecase{
+    constructor(
+
+    ){}
+    async execute(publicId: string, expiresIn: string): Promise<string> {
+        console.log("publice id",publicId)
+        if(!publicId || typeof publicId !== "string" ){
+            throw new CustomError(HTTP_STATUS.BAD_REQUEST,"Missing or invalid public id");
+        }
+
+        const url= await generateSignedUrl(publicId,+expiresIn) ;
+
+        return url;
+    }
+
+    async executeMultiple(publicIds: string[], expiresIn: number): Promise<string[]> {
+        if(!Array.isArray(publicIds) || publicIds.length === 0){
+            throw new CustomError(HTTP_STATUS.BAD_REQUEST,"Missing or invalid public ids array");
+        }
+
+        for(const publicId of publicIds){
+            if(!publicId || typeof publicId !=="string"){
+                throw new CustomError(HTTP_STATUS.BAD_REQUEST,'Invalid public id')
+            }
+        }
+
+        const urls = await Promise.all(publicIds.map(publicId => generateSignedUrl(publicId,expiresIn)));
+        return urls;
+    }
+}
