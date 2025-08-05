13eccb50d2a5cc36b3ee9849f59305bbaea3411f
SOURAV-RK <souravrk25@gmail.com>
2025-08-06 00:16:06 +0530

    added updateVendorProfile controller function

--- src/interfaceAdapters/controllers/vendor/vendor.profile.controller.ts
+++ src/interfaceAdapters/controllers/vendor/vendor.profile.controller.ts
@@ -5,12 +5,16 @@ import { Request, Response } from "express";
 import { IGetVendorDetailsUsecase } from "../../../entities/useCaseInterfaces/vendor/get-vendor-details-usecase.interface";
 import { HTTP_STATUS, SUCCESS_MESSAGE } from "../../../shared/constants";
 import { IUpdateVendorPasswordUsecase } from "../../../entities/useCaseInterfaces/vendor/update-vendor-password-usecase.interface";
+import { IUpdateVendorProfileUsecase } from "../../../entities/useCaseInterfaces/vendor/update-vendor-profile-usecase.interface";
 
 @injectable()
 export class VendorProfileController implements IVendorProfileController {
   constructor(
     @inject("IGetVendorDetailsUsecase")
     private _getVendorDetailsUsecase: IGetVendorDetailsUsecase,
+
+    @inject("IUpdateVendorProfileUsecase")
+    private _updateVendorProfileUsecase: IUpdateVendorProfileUsecase,
 
     @inject("IUpdateVendorPasswordUsecase")
     private _updateVendorPasswordUsecase: IUpdateVendorPasswordUsecase
@@ -22,6 +26,18 @@ export class VendorProfileController implements IVendorProfileController {
     res
       .status(HTTP_STATUS.OK)
       .json({ success: true, message: "fetched", vendor });
+  }
+
+  async updateVendorProfile(req: Request, res: Response): Promise<void> {
+    const vendorId = (req as CustomRequest).user.id;
+    const data = req.body;
+    await this._updateVendorProfileUsecase.execute(vendorId, data);
+    res
+      .status(HTTP_STATUS.OK)
+      .json({
+        success: true,
+        message: SUCCESS_MESSAGE.PROFILE_UPDATED_SUCCESS,
+      });
   }
 
   async updatePassword(req: Request, res: Response): Promise<void> {
