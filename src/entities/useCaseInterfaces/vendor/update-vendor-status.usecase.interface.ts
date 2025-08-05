36dbc033283a9ccf4948925d421d6512056b4f3d
SOURAV-RK <souravrk25@gmail.com>
2025-08-06 00:04:37 +0530

    removed successResponse handler

--- src/entities/useCaseInterfaces/vendor/update-vendor-status.usecase.interface.ts
+++ src/entities/useCaseInterfaces/vendor/update-vendor-status.usecase.interface.ts
@@ -1,5 +1,3 @@
-import { ISuccessResponseHandler } from "../../../shared/utils/successResponseHandler";
-
 export interface IUpdateVendorStatusUsecase{
     execute(vendorId : string,status : string) : Promise<void>;
 }
