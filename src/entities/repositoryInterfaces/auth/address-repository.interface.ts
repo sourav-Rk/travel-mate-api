8f8a6c6226b0062fd9aab391769aefd239a5dd74
SOURAV-RK <souravrk25@gmail.com>
2025-08-05 23:52:02 +0530

    added findByIdAndUpdate function

--- src/entities/repositoryInterfaces/auth/address-repository.interface.ts
+++ src/entities/repositoryInterfaces/auth/address-repository.interface.ts
@@ -3,4 +3,5 @@ import { IAddressEntity } from "../../modelsEntity/address.entity";
 export interface IAddressRepository{
     save(data : Partial<IAddressEntity>) : Promise<IAddressEntity>;
     findByUserId(vendorId : string) : Promise<IAddressEntity | null>;
+    findByIdAndUpdate(id : string,data : Partial<IAddressEntity>) : Promise<void>;
 }
