2d23a1389fd51ac84a330deb0252f0f72e9462e1
SOURAV-RK <souravrk25@gmail.com>
2025-08-13 20:00:09 +0530

    corrected configuration

--- src/frameworks/cloudinary/cloudinary.ts
+++ src/frameworks/cloudinary/cloudinary.ts
@@ -10,7 +10,7 @@ dotenv.config();
 cloudinary.config({
     cloud_name : config.cloudinary.cloud_name,
     api_key : config.cloudinary.api_key,
-    api_secret : config.cloudinary.api_secret 
+    api_secret : config.cloudinary.api_secret
 });
 
 const storage = new CloudinaryStorage({
