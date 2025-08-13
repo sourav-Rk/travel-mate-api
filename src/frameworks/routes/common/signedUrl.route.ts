12e4438fca42e9f1e5a2112be1a834f7b5c2cf13
SOURAV-RK <souravrk25@gmail.com>
2025-08-13 19:59:04 +0530

    added middleware

--- src/frameworks/routes/common/signedUrl.route.ts
+++ src/frameworks/routes/common/signedUrl.route.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 import { asyncHandler } from "../../../shared/async-handler";
 import { signedUrlController } from "../../di/resolve";
+import {
+  authorizeRole,
+  verifyAuth,
+} from "../../../interfaceAdapters/middlewares/auth.middleware";
 
 export class SignedUrlRoute {
   public router = Router();
@@ -13,6 +17,8 @@ export class SignedUrlRoute {
   private configureRoutes(): void {
     this.router.post(
       `/${this.role}/signed-url`,
+      verifyAuth,
+      authorizeRole([`${this.role}`]),
       asyncHandler(
         signedUrlController.generateSignedUrl.bind(signedUrlController)
       )
