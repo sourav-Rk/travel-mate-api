8d5226d0f27aa19eb7b8e54e806525ec85a3a4ee
SOURAV-RK <souravrk25@gmail.com>
2025-08-19 16:57:27 +0530

    added itineray controller

---
+++ src/interfaceAdapters/controllers/itinerary/itinerary.controller.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from "tsyringe";
+import { IItineraryController } from "../../../entities/controllerInterfaces/itinerary/itinerary-controller.interface";
+import { IUpdateItineraryUsecase } from "../../../entities/useCaseInterfaces/itinerary/updateItinerary-usecase.interface";
+import { Request, Response } from "express";
+import { HTTP_STATUS, SUCCESS_MESSAGE } from "../../../shared/constants";
+
+@injectable()
+export class ItineraryController implements IItineraryController{
+    constructor(
+      @inject('IUpdateItineraryUsecase')
+      private _updateItineraryUsecase : IUpdateItineraryUsecase
+    ){}
+
+    async updateItinerary(req: Request, res: Response): Promise<void> {
+        const {id} = req.params;
+        const itineraryData = req.body;
+        await this._updateItineraryUsecase.execute(id,itineraryData);
+        res.status(HTTP_STATUS.OK).json({success : true,message : SUCCESS_MESSAGE.ITINERARY_UPDATED_SUCCESS});
+    }
+}
