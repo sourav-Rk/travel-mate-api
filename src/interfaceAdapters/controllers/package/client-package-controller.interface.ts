d931864732c1fc05e62ab69bfd235dd45fdd5215
SOURAV-RK <souravrk25@gmail.com>
2025-08-20 10:41:25 +0530

    added client package controllers

---
+++ src/interfaceAdapters/controllers/package/client-package-controller.interface.ts
@@ -0,0 +1,95 @@
+import { inject, injectable } from "tsyringe";
+import { IClientPackageController } from "../../../entities/controllerInterfaces/package/client-package.controller";
+import { Request, Response } from "express";
+import { IGetAvailablePackagesUsecase } from "../../../entities/useCaseInterfaces/package/client-package/getAvailable-package-usecase.interface";
+import { HTTP_STATUS } from "../../../shared/constants";
+import { IGetPackageDetailsClientUsecase } from "../../../entities/useCaseInterfaces/package/client-package/getPackageDetailsClient-usecase.interface";
+import { IGetFeaturedPackagesUsecase } from "../../../entities/useCaseInterfaces/package/client-package/getFeaturedPackages-usecase.interface";
+
+@injectable()
+export class ClientPackageController implements IClientPackageController {
+  constructor(
+    @inject("IGetAvailablePackagesUsecase")
+    private _getAvailablePackageUsecase: IGetAvailablePackagesUsecase,
+
+    @inject("IGetPackageDetailsClientUsecase")
+    private _getPackageDetailsClientUsecase: IGetPackageDetailsClientUsecase,
+
+    @inject("IGetFeaturedPackagesUsecase")
+    private _getFeaturedPackagesUsecase : IGetFeaturedPackagesUsecase
+  ) {}
+
+  async getAvailablePackages(req: Request, res: Response): Promise<void> {
+    const {
+      page = "1",
+      limit = "10",
+      search = "",
+      priceRange,
+      duration = "",
+      sortBy = "createdAt",
+    } = req.query as {
+      page?: string;
+      limit?: string;
+      search?: string;
+      priceRange?: string[] | string;
+      duration?: string;
+      sortBy?: string;
+    };
+
+    let categories: string[] = [];
+
+    if (req.query.categories) {
+      const category = req.query.categories;
+      categories = Array.isArray(category)
+        ? category.map(String)
+        : [String(category)];
+    }
+
+    const parsedPriceRange: number[] = [];
+    if (priceRange) {
+      const range = Array.isArray(priceRange) ? priceRange : [priceRange];
+      parsedPriceRange.push(...range.map(Number));
+    }
+
+    const pageNumber = Number(page);
+    const pageSize = Number(limit);
+    const { packages, total } = await this._getAvailablePackageUsecase.execute(
+      search,
+      categories,
+      parsedPriceRange,
+      duration,
+      pageNumber,
+      pageSize,
+      sortBy
+    );
+    res.status(HTTP_STATUS.OK).json({
+      success: true,
+      packages,
+      totalPages: total,
+      currentPage: pageNumber,
+    });
+  }
+
+  async getFeaturedPackages(req: Request, res: Response): Promise<void> {
+        const packageId = req.params.packageId;
+    
+    // Validate that packageId is a string
+    if (typeof packageId !== 'string') {
+       res.status(400).json({
+        success: false,
+        message: 'Invalid package ID'
+      });
+      return 
+    }
+    const response = await this._getFeaturedPackagesUsecase.execute(packageId);
+     res.status(HTTP_STATUS.OK).json({success : true,packages : response});
+  }
+
+  async getPackageDetails(req: Request, res: Response): Promise<void> {
+    const { packageId } = req.params;
+    const response = await this._getPackageDetailsClientUsecase.execute(
+      packageId
+    );
+    res.status(HTTP_STATUS.OK).json({ success: true, packages: response });
+  }
+}
