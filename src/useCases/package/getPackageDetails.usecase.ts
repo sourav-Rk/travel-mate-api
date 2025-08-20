6c0b74c9194f5b510c748ea4fbb2fee0548558e3
SOURAV-RK <souravrk25@gmail.com>
2025-08-20 10:37:29 +0530

    added userType as parameter

--- src/useCases/package/getPackageDetails.usecase.ts
+++ src/useCases/package/getPackageDetails.usecase.ts
@@ -6,29 +6,49 @@ import { ERROR_MESSAGE } from "../../shared/constants";
 import { IVendorRepository } from "../../entities/repositoryInterfaces/vendor/vendor-repository.interface";
 import { NotFoundError } from "../../shared/utils/error/notFoundError";
 import { IPackage } from "../../shared/dto/packageDto";
+import { IAdminRepository } from "../../entities/repositoryInterfaces/admin/admin-repository.interface";
 
 @injectable()
-export class GetPackageDetailsUsecase implements IGetPackageDetailsUsecase{
-    constructor(
-        @inject('IPackageRepository')
-        private _packageRepository : IPackageRepository,
+export class GetPackageDetailsUsecase implements IGetPackageDetailsUsecase {
+  constructor(
+    @inject("IPackageRepository")
+    private _packageRepository: IPackageRepository,
 
-        @inject('IVendorRepository')
-        private _vendorRepository : IVendorRepository
-    ){}
+    @inject("IVendorRepository")
+    private _vendorRepository: IVendorRepository,
 
-    async execute(agencyId: string, packageId: string): Promise<IPackage> {
-        if(!agencyId || !packageId){
-            throw new ValidationError(ERROR_MESSAGE.AGENCY_ID_AND_PACKAGE_ID_IS_REQUIRED);
-        }
+    @inject("IAdminRepository")
+    private _adminRepository: IAdminRepository
+  ) {}
 
-        const agency = await this._vendorRepository.findById(agencyId);
-
-        if(!agency){
-            throw new NotFoundError(ERROR_MESSAGE.AGENCY_NOT_FOUND)
-        }
-
-        const response = await this._packageRepository.getPackageDetails(packageId);
-        return response;
+  async execute(
+    userType: string,
+    userId: string,
+    packageId: string
+  ): Promise<IPackage> {
+    if (!userId || !packageId) {
+      throw new ValidationError(
+        ERROR_MESSAGE.AGENCY_ID_AND_PACKAGE_ID_IS_REQUIRED
+      );
     }
+
+    if (userType === "vendor") {
+      const agency = await this._vendorRepository.findById(userId);
+
+      if (!agency) {
+        throw new NotFoundError(ERROR_MESSAGE.AGENCY_NOT_FOUND);
+      }
+    }
+
+    if (userType === "admin") {
+      const admin = await this._adminRepository.findById(userId);
+
+      if (!admin) {
+        throw new NotFoundError(ERROR_MESSAGE.AGENCY_NOT_FOUND);
+      }
+    }
+
+    const response = await this._packageRepository.getPackageDetails(packageId);
+    return response;
+  }
 }
