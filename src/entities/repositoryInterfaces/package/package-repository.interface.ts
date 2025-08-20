0e7a4ebf8210d5e2f359201742bae41ae6099374
SOURAV-RK <souravrk25@gmail.com>
2025-08-20 10:35:29 +0530

    corrected the repositories

--- src/entities/repositoryInterfaces/package/package-repository.interface.ts
+++ src/entities/repositoryInterfaces/package/package-repository.interface.ts
@@ -3,17 +3,21 @@ import { IPackageEntity } from "../../modelsEntity/package.entity";
 import { PaginatedPackages } from "../../modelsEntity/paginated-packages.entity";
 
 export interface IPackageRepository {
-  findById(id : string) : Promise<IPackageEntity | null>
+  findById(id: string): Promise<IPackageEntity | null>;
   save(data: IPackageEntity, session?: any): Promise<IPackageEntity>;
   update(
     id: any,
     data: Partial<IPackageEntity>,
     session?: any
   ): Promise<IPackageEntity>;
-  find(
+  find(skip: number, limit: number, filter: any): Promise<PaginatedPackages>;
+  getPackageDetails(id: any): Promise<IPackage>;
+  getActivePackages(
     skip: number,
     limit: number,
-    filter: any
+    filter: any,
+    sort: any
   ): Promise<PaginatedPackages>;
-  getPackageDetails(id : any) : Promise<IPackage>;
+
+  getFeaturedPackages(category : string) : Promise<IPackageEntity[]>
 }
