4b6b8fb323cc4b299653cb4628ac7eead469a97b
SOURAV-RK <souravrk25@gmail.com>
2025-08-20 10:40:06 +0530

    added client package controller interfaces

---
+++ src/entities/controllerInterfaces/package/client-package.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export interface IClientPackageController {
+    getAvailablePackages(req : Request,res : Response) : Promise<void>;
+    getPackageDetails(req : Request,res : Response) : Promise<void>;
+    getFeaturedPackages(req : Request,res : Response) : Promise<void>;
+}
