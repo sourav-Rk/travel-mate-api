50a4c9a30dcfd71d35ed93df1a8c7e6df487b36d
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:15:57 +0530

    omited the _id and agency id and override it with object id

--- src/frameworks/database/models/package.model.ts
+++ src/frameworks/database/models/package.model.ts
@@ -1,7 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 import { IPackageEntity } from "../../../entities/modelsEntity/package.entity";
 import { packageSchema } from "../schemas/package.schema";
 
-export interface IPackageModel extends IPackageEntity{}
-export const packageDB = mongoose.model("packages",packageSchema)
+export interface IPackageModel
+  extends Omit<IPackageEntity, "_id" | "agencyId">,
+    Document {
+  agencyId: mongoose.Types.ObjectId;
+}
+export const packageDB = mongoose.model("packages", packageSchema);
