fe6c7d802ba363b01feee49867bb7b7f9b787a8a
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:40:16 +0530

    added Ipackagerepositor to check the status of the package

--- src/useCases/activity/deleteActivity.usecase.ts
+++ src/useCases/activity/deleteActivity.usecase.ts
@@ -1,10 +1,13 @@
 import { inject, injectable } from "tsyringe";
-import {  IDeleteActivityUsecase } from "../../entities/useCaseInterfaces/activity/deleteActivity-usecase.interface";
+
 import { IActivitiesRepository } from "../../entities/repositoryInterfaces/package/activities-repository.interface";
-import { ValidationError } from "../../shared/utils/error/validationError";
-import { ERROR_MESSAGE } from "../../shared/constants";
-import { NotFoundError } from "../../shared/utils/error/notFoundError";
 import { IItineraryRepository } from "../../entities/repositoryInterfaces/package/itinerary-repository.interface";
+import { IPackageRepository } from "../../entities/repositoryInterfaces/package/package-repository.interface";
+import {  IDeleteActivityUsecase } from "../../entities/useCaseInterfaces/activity/deleteActivity-usecase.interface";
+import { ERROR_MESSAGE, HTTP_STATUS } from "../../shared/constants";
+import { CustomError } from "../../shared/utils/error/customError";
+import { NotFoundError } from "../../shared/utils/error/notFoundError";
+import { ValidationError } from "../../shared/utils/error/validationError";
 
 @injectable()
 export class DeleteActivityUsecase implements IDeleteActivityUsecase{
@@ -13,12 +16,21 @@ export class DeleteActivityUsecase implements IDeleteActivityUsecase{
         private _activitiesRepository : IActivitiesRepository,
 
         @inject('IItineraryRepository')
-        private _itinerayRepository : IItineraryRepository
+        private _itinerayRepository : IItineraryRepository,
+
+        @inject('IPackageRepository')
+        private _packageRepository : IPackageRepository
     ){}
 
     async execute(itineraryId: string, dayNumber: number, id: string): Promise<void> {
          if(!id || !itineraryId){
             throw new ValidationError(ERROR_MESSAGE.ID_REQUIRED);
+        }
+
+        const packageDetails = await this._packageRepository.findByItineraryId(itineraryId);
+
+        if(packageDetails?.status !== "draft"){
+            throw new CustomError(HTTP_STATUS.CONFLICT,ERROR_MESSAGE.CANNOT_EDIT_PACKAGE+` ${packageDetails?.status}`)
         }
 
         const activityExist = await this._activitiesRepository.findById(id);
