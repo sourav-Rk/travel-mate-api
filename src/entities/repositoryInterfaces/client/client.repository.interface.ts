ee6519314706556728ef729e16656134e1a36e06
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:00:15 +0530

    added arguments

--- src/entities/repositoryInterfaces/client/client.repository.interface.ts
+++ src/entities/repositoryInterfaces/client/client.repository.interface.ts
@@ -1,3 +1,4 @@
+import { TRole } from "../../../shared/constants";
 import { IClientEntity } from "../../modelsEntity/client.entity";
 
 export interface IClientRepository {
@@ -5,15 +6,20 @@ export interface IClientRepository {
   findById(id: string): Promise<IClientEntity | null>;
   findByEmail(email: string): Promise<IClientEntity | null>;
   findByNumber(phone: string): Promise<IClientEntity | null>;
-  findByIdAndUpdatePassword(id : any,password : string) : Promise<IClientEntity | null>;
+  findByIdAndUpdatePassword(
+    id: any,
+    password: string
+  ): Promise<IClientEntity | null>;
   findByIdAndUpdateStatus(id: string): Promise<boolean>;
   updateClientProfileById(
     id: string,
     data: Partial<IClientEntity>
   ): Promise<void>;
   find(
-    filter: any,
-    skip: number,
-    limit: number
+    searchTerm: string,
+    status: string,
+    userType: TRole,
+    validPageNumber: number,
+    validPageSize: number
   ): Promise<{ user: IClientEntity[] | []; total: number }>;
 }
