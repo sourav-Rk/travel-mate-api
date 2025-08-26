bf83c79a93459fc633ce3c224dcb8ad8d788f69a
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:44:30 +0530

    added mapper

--- src/interfaceAdapters/repositories/package/itinerary.repository.ts
+++ src/interfaceAdapters/repositories/package/itinerary.repository.ts
@@ -1,37 +1,62 @@
 import { injectable } from "tsyringe";
-import { IItineraryRepository } from "../../../entities/repositoryInterfaces/package/itinerary-repository.interface";
+
 import { IItineraryEntity } from "../../../entities/modelsEntity/itinerary.entity";
+import { IItineraryRepository } from "../../../entities/repositoryInterfaces/package/itinerary-repository.interface";
 import { itineraryDB } from "../../../frameworks/database/models/itinerary.model";
 import { ItineraryEditDto } from "../../../shared/dto/itineraryDto";
+import { ItineraryMapper } from "../../mappers/itinerary.mapper";
 
 @injectable()
 export class ItineraryRepository implements IItineraryRepository {
   async save(data: IItineraryEntity, session?: any): Promise<IItineraryEntity> {
     const options = session ? { session } : {};
-    return await itineraryDB
+    const modelData = await itineraryDB
       .create([data], options)
       .then((result) => result[0]);
+
+    return ItineraryMapper.toEntity(modelData);
   }
 
   async findById(id: string): Promise<IItineraryEntity | null> {
-      return await itineraryDB.findById(id);
+    return await itineraryDB.findById(id);
   }
 
   async findByPackageId(packageId: string): Promise<IItineraryEntity | null> {
-      return await itineraryDB.findOne({packageId})
+    return await itineraryDB.findOne({ packageId });
   }
 
-  async update(id: string, data: ItineraryEditDto): Promise<IItineraryEntity | null> {
-      return await itineraryDB.findByIdAndUpdate(id,{$set : data},{new: true,runValidators:true})
+  async update(
+    id: string,
+    data: ItineraryEditDto
+  ): Promise<IItineraryEntity | null> {
+    return await itineraryDB.findByIdAndUpdate(
+      id,
+      { $set: data },
+      { new: true, runValidators: true }
+    );
   }
 
-  async addActivityToDay(itineraryId: string, dayNumber: number, activityId: string): Promise<boolean> {
-      const result =  await itineraryDB.updateOne({_id : itineraryId,"days.dayNumber":dayNumber},{$addToSet : {'days.$.activities' : activityId}});
-      return result.modifiedCount > 0;
+  async addActivityToDay(
+    itineraryId: string,
+    dayNumber: number,
+    activityId: string
+  ): Promise<boolean> {
+    const result = await itineraryDB.updateOne(
+      { _id: itineraryId, "days.dayNumber": dayNumber },
+      { $addToSet: { "days.$.activities": activityId } }
+    );
+    return result.modifiedCount > 0;
   }
 
-  async removeActivityFromDay(itineraryId: string, dayNumber: number, activityId: string): Promise<boolean> {
-      const result = await itineraryDB.updateOne({_id : itineraryId,"days.dayNumber" : dayNumber},{$pull : {'days.$.activities':activityId}});
-      return result.modifiedCount > 0;
+  async removeActivityFromDay(
+    itineraryId: string,
+    dayNumber: number,
+    activityId: string
+  ): Promise<boolean> {
+    const result = await itineraryDB.updateOne(
+      { _id: itineraryId, "days.dayNumber": dayNumber },
+      { $pull: { "days.$.activities": activityId } }
+    );
+    return result.modifiedCount > 0;
   }
 }
