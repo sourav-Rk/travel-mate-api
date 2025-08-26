94ad9c34531033e942b04a0e87a11c1c1a002324
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:44:02 +0530

    formatted the code

--- src/interfaceAdapters/repositories/package/package.repository.ts
+++ src/interfaceAdapters/repositories/package/package.repository.ts
@@ -1,10 +1,15 @@
-import { injectable } from "tsyringe";
-import { packageDB } from "../../../frameworks/database/models/package.model";
-import { IPackageRepository } from "../../../entities/repositoryInterfaces/package/package-repository.interface";
-import { IPackageEntity } from "../../../entities/modelsEntity/package.entity";
-import { PaginatedPackages } from "../../../entities/modelsEntity/paginated-packages.entity";
 import mongoose from "mongoose";
-import { IPackage } from "../../../shared/dto/packageDto";
+import { injectable } from "tsyringe";
+
+import { IPackageEntity } from "../../../entities/modelsEntity/package.entity";
+import { IPackageRepository } from "../../../entities/repositoryInterfaces/package/package-repository.interface";
+import { packageDB } from "../../../frameworks/database/models/package.model";
+import { PackageStatus, TRole } from "../../../shared/constants";
+import {
+  IPackage,
+  PaginatedPackagesRepo,
+} from "../../../shared/dto/packageDto";
+import { PackageMapper } from "../../mappers/package.mapper";
 
 @injectable()
 export class PackageRepository implements IPackageRepository {
@@ -12,9 +17,16 @@ export class PackageRepository implements IPackageRepository {
     return await packageDB.findById(id);
   }
 
+  async findByItineraryId(id: string): Promise<IPackageEntity | null> {
+    return await packageDB.findOne({ itineraryId: id });
+  }
+
   async save(data: IPackageEntity, session?: any): Promise<IPackageEntity> {
     const options = session ? { session } : {};
-    return await packageDB.create([data], options).then((result) => result[0]);
+    const modelData = await packageDB
+      .create([data], options)
+      .then((result) => result[0]);
+    return PackageMapper.toEntity(modelData);
   }
 
   async update(
@@ -31,24 +43,89 @@ export class PackageRepository implements IPackageRepository {
       throw new Error(`Package with id ${id} not found`);
     }
 
-    return result;
+    return PackageMapper.toEntity(result);
   }
 
-  async find(
-    skip: number,
-    limit: number,
-    filter: any
-  ): Promise<PaginatedPackages> {
+  async updatePackageStatus(
+    packageId: string,
+    status: PackageStatus
+  ): Promise<IPackageEntity | null> {
+    return await packageDB.findByIdAndUpdate(
+      packageId,
+      { $set: { status: status } },
+      { new: true }
+    );
+  }
+
+  async updateBlock(
+    packageId: string,
+    isBlocked: boolean
+  ): Promise<IPackageEntity | null> {
+    return await packageDB.findByIdAndUpdate(packageId, {
+      $set: { isBlocked: !isBlocked },
+    });
+  }
+
+  async find(criteria: {
+    userId: any;
+    userType: TRole;
+    searchTerm: string;
+    status: string;
+    category: string;
+    pageNumber: number;
+    pageSize: number;
+  }): Promise<PaginatedPackagesRepo> {
+    const {
+      userId,
+      userType,
+      searchTerm,
+      status,
+      category,
+      pageNumber,
+      pageSize,
+    } = criteria;
+
+    const filter: any = {};
+
+    if (userType === "vendor") {
+      filter.agencyId = userId;
+    }
+
+    if (searchTerm) {
+      filter.$or = [
+        { packageName: { $regex: searchTerm, $options: "i" } },
+        { title: { $regex: searchTerm, $options: "i" } },
+        { tags: { $regex: searchTerm, $options: "i" } },
+      ];
+    }
+
+    if (category && category !== "all") {
+      filter.category = category;
+    }
+
+    if (status && status !== "all" && status !== "blocked") {
+      filter.status = status;
+    }
+
+    if (status === "blocked") {
+      filter.isBlocked = true;
+    }
+
+    const skip = (pageNumber - 1) * pageSize;
+    const limit = pageSize;
+
     const [packages, total] = await Promise.all([
       await packageDB
         .find(filter)
         .skip(skip)
         .limit(limit)
-        .sort({ createdAt: -1 }),
+        .sort({ createdAt: 1 }),
       await packageDB.countDocuments(filter),
     ]);
 
-    return { packages, total };
+    const packageDetails = packages.map((doc) => PackageMapper.toEntity(doc));
+
+    return { packages: packageDetails, total };
   }
 
   async getPackageDetails(id: any): Promise<IPackage> {
@@ -94,25 +171,79 @@ export class PackageRepository implements IPackageRepository {
   }
 
   async getActivePackages(
-    skip: number,
-    limit: number,
-    filter: any,
-    sort: any
-  ): Promise<PaginatedPackages> {
+    search: string,
+    categories: string[],
+    priceRange: number[],
+    duration: string,
+    pageNumber: number,
+    pageSize: number,
+    sortBy: string
+  ): Promise<PaginatedPackagesRepo> {
+    const filter: any = { status: "active", isBlocked: false };
+
+    if (search) {
+      filter.$or = [
+        { packageName: { $regex: search, $options: "i" } },
+        { title: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    if (categories && categories.length > 0) {
+      filter.category = {
+        $in: categories.map((cat) => new RegExp(`^${cat}$`, "i")),
+      };
+    }
+
+    if (priceRange && priceRange.length === 2) {
+      filter.price = { $gte: priceRange[0], $lte: priceRange[1] };
+    }
+
+    if (duration && duration.includes("-")) {
+      const [min, max] = duration.split("-").map(Number);
+      filter["duration.days"] = { $gte: min, $lte: max };
+    }
+
+    //pagination
+    const skip = (pageNumber - 1) * pageSize;
+    const limit = pageSize;
+
+    //sorting
+    const sortField: any = {};
+    if (sortBy === "price-high") sortField.price = -1;
+    else if (sortBy === "price-low") sortField.price = 1;
+    else if (sortBy === "days-long") sortField["duration.days"] = -1;
+    else if (sortBy === "days-short") sortField["duration.days"] = 1;
+    else sortField.createdAt = -1;
+
     const [packages, total] = await Promise.all([
-      await packageDB.find(filter).skip(skip).limit(limit).sort(sort),
+      await packageDB.find(filter).skip(skip).limit(limit).sort(sortField),
       await packageDB.countDocuments(filter),
     ]);
+    
+    const packageDetails = packages.map((doc) => PackageMapper.toEntity(doc));
 
-    return { packages, total };
+    return { packages : packageDetails, total };
   }
 
   async getFeaturedPackages(category: string): Promise<IPackageEntity[]> {
-    return await packageDB.find({
-      category: {
-        $regex: `^${category}$`,
-        $options: "i",
-      },
-    }).limit(3);
+    const packages = await packageDB
+      .find({
+        category: {
+          $regex: `^${category}$`,
+          $options: "i",
+        },
+      })
+      .limit(3);
+
+    return packages.map((doc) => PackageMapper.toEntity(doc));
+  }
+
+  async getTrendingPackages(): Promise<IPackageEntity[]> {
+    const packages =  await packageDB
+      .find({ status: "active" })
+      .sort({ createdAt: -1 })
+      .limit(6);
+
+      return packages.map((doc) => PackageMapper.toEntity(doc));
   }
 }
