cb8097d42e251287b516b6ad72af4cb07176af9c
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:12:04 +0530

    omitted the userId and extended it with objectid

--- src/frameworks/database/models/address.model.ts
+++ src/frameworks/database/models/address.model.ts
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 import { IAddressEntity } from "../../../entities/modelsEntity/address.entity";
 import { addressSchema } from "../schemas/address.schema";
 
-export interface IAddressModel extends IAddressEntity{}
+export interface IAddressModel extends Omit<IAddressEntity,"userId">, Document {
+    userId : mongoose.Types.ObjectId
+}
 export const addressDB = mongoose.model("address",addressSchema)
