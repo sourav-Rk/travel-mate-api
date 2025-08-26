8716ab2f1a876a70c6af6f30d15f9d171f728c6b
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:01:34 +0530

    added arguments for filtering

--- src/entities/repositoryInterfaces/package/package-repository.interface.ts
+++ src/entities/repositoryInterfaces/package/package-repository.interface.ts
@@ -1,23 +1,38 @@
-import { IPackage } from "../../../shared/dto/packageDto";
+import { PackageStatus, TRole } from "../../../shared/constants";
+import { IPackage, PaginatedPackagesRepo } from "../../../shared/dto/packageDto";
 import { IPackageEntity } from "../../modelsEntity/package.entity";
-import { PaginatedPackages } from "../../modelsEntity/paginated-packages.entity";
 
 export interface IPackageRepository {
   findById(id: string): Promise<IPackageEntity | null>;
+  findByItineraryId(id : string) : Promise<IPackageEntity | null>;
   save(data: IPackageEntity, session?: any): Promise<IPackageEntity>;
   update(
     id: any,
     data: Partial<IPackageEntity>,
     session?: any
   ): Promise<IPackageEntity>;
-  find(skip: number, limit: number, filter: any): Promise<PaginatedPackages>;
+  updatePackageStatus(packageId : string,status : PackageStatus) : Promise<IPackageEntity | null>;
+  find(
+    criteria : {userId: any,
+    userType: TRole,
+    searchTerm: string,
+    status: string,
+    category: string,
+    pageNumber: number,
+    pageSize: number}
+  ): Promise<PaginatedPackagesRepo>;
   getPackageDetails(id: any): Promise<IPackage>;
+  updateBlock(packageId : string,isBlocked : boolean) : Promise<IPackageEntity | null>;
   getActivePackages(
-    skip: number,
-    limit: number,
-    filter: any,
-    sort: any
-  ): Promise<PaginatedPackages>;
+    search : string,
+    categories : string [],
+    priceRange : number[],
+    duration : string,
+    pageNumber : number,
+    pageSize : number,
+    sortBy : string
+  ): Promise<PaginatedPackagesRepo>;
+  getTrendingPackages() : Promise<IPackageEntity[]>
 
-  getFeaturedPackages(category : string) : Promise<IPackageEntity[]>
+  getFeaturedPackages(category: string): Promise<IPackageEntity[]>;
 }
