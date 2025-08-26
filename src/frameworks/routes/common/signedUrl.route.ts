661f627847a547daf37c6544c475f9f30cf59676
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:55:05 +0530

    removed eslint errors

--- src/frameworks/routes/common/signedUrl.route.ts
+++ src/frameworks/routes/common/signedUrl.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
-import { asyncHandler } from "../../../shared/async-handler";
-import { signedUrlController } from "../../di/resolve";
 import {
   authorizeRole,
   verifyAuth,
 } from "../../../interfaceAdapters/middlewares/auth.middleware";
+import { asyncHandler } from "../../../shared/async-handler";
+import { signedUrlController } from "../../di/resolve";
 
 export class SignedUrlRoute {
   public router = Router();
