f932845cdcddd45c536e5f341a1e1b42d6edd754
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:13:32 +0530

    omitted the _id and extended it as object id

--- src/frameworks/database/models/client.model.ts
+++ src/frameworks/database/models/client.model.ts
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { Document, ObjectId } from "mongoose";
 
 import { IClientEntity } from "../../../entities/modelsEntity/client.entity";
 import { clientSchema } from "../schemas/client.schema";
 
 
-export interface IClientModel extends IClientEntity{}
+export interface IClientModel extends Omit<IClientEntity,"_id">,Document{
+    _id : ObjectId;
+}
 export const clientDB = mongoose.model("clients",clientSchema)
