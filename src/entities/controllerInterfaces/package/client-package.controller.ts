4d0b6c33d95d373ce8a33e8bbcda7a6f6102e77d
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 19:01:25 +0530

    added trending package usecases

--- src/entities/controllerInterfaces/package/client-package.controller.ts
+++ src/entities/controllerInterfaces/package/client-package.controller.ts
@@ -4,4 +4,5 @@ export interface IClientPackageController {
     getAvailablePackages(req : Request,res : Response) : Promise<void>;
     getPackageDetails(req : Request,res : Response) : Promise<void>;
     getFeaturedPackages(req : Request,res : Response) : Promise<void>;
+    getTrendingPackages(req : Request,res : Response) : Promise<void>;
 }
