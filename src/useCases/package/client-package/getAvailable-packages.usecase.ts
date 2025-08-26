057ce31f18402e0d9a7d81459a86466667a61f95
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:29:42 +0530

    corrected usecase

--- src/useCases/package/client-package/getAvailable-packages.usecase.ts
+++ src/useCases/package/client-package/getAvailable-packages.usecase.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from "tsyringe";
-import { IGetAvailablePackagesUsecase } from "../../../entities/useCaseInterfaces/package/client-package/getAvailable-package-usecase.interface";
+
 import { IPackageRepository } from "../../../entities/repositoryInterfaces/package/package-repository.interface";
-import { PaginatedPackages } from "../../../entities/modelsEntity/paginated-packages.entity";
-import { any } from "zod";
+import { IGetAvailablePackagesUsecase } from "../../../entities/useCaseInterfaces/package/client-package/getAvailable-package-usecase.interface";
+import { PackageMapper } from "../../../interfaceAdapters/mappers/package.mapper";
+import { PaginatedPackagesUserSideListing } from "../../../shared/dto/packageDto";
 
 @injectable()
 export class GetAvailbalePackagesUsecase
@@ -21,55 +22,28 @@ export class GetAvailbalePackagesUsecase
     pageNumber: number,
     pageSize: number,
     sortBy: string
-  ): Promise<PaginatedPackages> {
-    let filter: any = { status: "active" };
-
-    if (search) {
-      filter.$or = [
-        { packageName: { $regex: search, $options: "i" } },
-        { title: { $regex: search, $options: "i" } },
-      ];
-    }
-
-    if (categories && categories.length > 0) {
-      filter.category = {
-        $in: categories.map((cat) => new RegExp(`^${cat}$`, "i")),
-      };
-    }
-
-    if (priceRange && priceRange.length === 2) {
-      filter.price = { $gte: priceRange[0], $lte: priceRange[1] };
-    }
-
-    if (duration && duration.includes("-")) {
-      const [min, max] = duration.split("-").map(Number);
-      filter["duration.days"] = { $gte: min, $lte: max };
-    }
-
+  ): Promise<PaginatedPackagesUserSideListing> {
     //pagination
     const validPageNumber = Math.max(1, pageNumber || 0);
     const validPageSize = Math.max(1, pageSize || 10);
-    const skip = (validPageNumber - 1) * validPageSize;
-    const limit = validPageSize;
-
-    //sorting
-    const sortField: any = {};
-    if (sortBy === "price-high") sortField.price = -1;
-    else if (sortBy === "price-low") sortField.price = 1;
-    else if (sortBy === "days-long") sortField["duration.days"] = -1;
-    else if (sortBy === "days-short") sortField["duration.days"] = 1;
-    else sortField.createdAt = -1;
 
     const { packages, total } =
       await this._packagesRepository.getActivePackages(
-        skip,
-        limit,
-        filter,
-        sortField
+        search,
+        categories,
+        priceRange,
+        duration,
+        validPageNumber,
+        validPageSize,
+        sortBy
       );
 
-    const response: PaginatedPackages = {
-      packages,
+    const packageDetails = packages.map((doc) =>
+      PackageMapper.mapPackageToUserListingDto(doc)
+    );
+
+    const response = {
+      packages: packageDetails,
       total: Math.ceil(total / validPageSize),
     };
     return response;
