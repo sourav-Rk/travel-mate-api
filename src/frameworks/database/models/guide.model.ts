0ac88f3a8a70ccad104188ee714657224924cd16
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:14:15 +0530

    omitted the agencyId and extended it with object id

--- src/frameworks/database/models/guide.model.ts
+++ src/frameworks/database/models/guide.model.ts
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 import { IGuideEntity } from "../../../entities/modelsEntity/guide.entity";
 import { guideSchema } from "../schemas/guide.schema";
 
-export interface IGuideModel extends IGuideEntity{}
-export const guideDB = mongoose.model("guides",guideSchema)
+export interface IGuideModel extends Omit<IGuideEntity, "agencyId"> {
+  agencyId: mongoose.Types.ObjectId;
+}
+export const guideDB = mongoose.model("guides", guideSchema);
