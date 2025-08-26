458b3808d537216ce333e5f617ef976dfae1241d
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:26:16 +0530

    added mapper

--- src/useCases/vendor/get-vendor-details.usecase.ts
+++ src/useCases/vendor/get-vendor-details.usecase.ts
@@ -1,31 +1,37 @@
 import { inject, injectable } from "tsyringe";
 
-import { IVendorEntity } from "../../entities/modelsEntity/vendor.entity";
 import { IVendorRepository } from "../../entities/repositoryInterfaces/vendor/vendor-repository.interface";
 import { IGetVendorDetailsUsecase } from "../../entities/useCaseInterfaces/vendor/get-vendor-details-usecase.interface";
+import { VendorMapper } from "../../interfaceAdapters/mappers/vendor.mapper";
 import { ERROR_MESSAGE, HTTP_STATUS } from "../../shared/constants";
+import { VendorProfileDto } from "../../shared/dto/vendor.dto";
 import { CustomError } from "../../shared/utils/error/customError";
 
 @injectable()
 export class GetVendorDetailsUsecase implements IGetVendorDetailsUsecase {
-    constructor(
-        @inject('IVendorRepository')
-        private _vendorRepository : IVendorRepository
-    ){}
+  constructor(
+    @inject("IVendorRepository")
+    private _vendorRepository: IVendorRepository
+  ) {}
 
-    async execute(id: any): Promise<IVendorEntity | null> {
-        if(!id) {
-            throw new CustomError(HTTP_STATUS.BAD_REQUEST,"Vendor id is missing");
-        }
-
-        const vendor = await this._vendorRepository.findById(id);
-
-        if(!vendor){
-            throw new CustomError(HTTP_STATUS.BAD_REQUEST,ERROR_MESSAGE.USER_NOT_FOUND);
-        }
-
-        const vendorDetails = await this._vendorRepository.getVendorWithAddressAndKyc(id);
-
-        return vendorDetails;
+  async execute(id: any): Promise<VendorProfileDto | null> {
+    if (!id) {
+      throw new CustomError(HTTP_STATUS.BAD_REQUEST, "Vendor id is missing");
     }
+
+    const vendor = await this._vendorRepository.findById(id);
+
+    if (!vendor) {
+      throw new CustomError(
+        HTTP_STATUS.BAD_REQUEST,
+        ERROR_MESSAGE.USER_NOT_FOUND
+      );
+    }
+
+    const vendorDetails = VendorMapper.mapVendorToFullInfoDto(
+      await this._vendorRepository.getVendorWithAddressAndKyc(id)
+    );
+
+    return vendorDetails;
+  }
 }
