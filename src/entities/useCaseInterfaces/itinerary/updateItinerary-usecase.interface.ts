08f9f5ec67dece036a200df49d3869c9afc0a2cd
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:05:02 +0530

    changed itineraryEntity to dto

--- src/entities/useCaseInterfaces/itinerary/updateItinerary-usecase.interface.ts
+++ src/entities/useCaseInterfaces/itinerary/updateItinerary-usecase.interface.ts
@@ -1,5 +1,4 @@
 import { ItineraryEditDto } from "../../../shared/dto/itineraryDto";
-import { IItineraryEntity } from "../../modelsEntity/itinerary.entity";
 
 export interface IUpdateItineraryUsecase{
     execute(itineraryId : string,data : ItineraryEditDto) : Promise<void>;
