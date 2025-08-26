8eff255cb951b6854c2f50c48937653fdf65f593
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:03:38 +0530

    changed to dto

--- src/entities/useCaseInterfaces/admin/getUserById-usecase.interface.ts
+++ src/entities/useCaseInterfaces/admin/getUserById-usecase.interface.ts
@@ -1,6 +1,6 @@
+import { VendorProfileDto } from "../../../shared/dto/vendor.dto";
 import { IClientEntity } from "../../modelsEntity/client.entity";
-import { IVendorEntity } from "../../modelsEntity/vendor.entity";
 
 export interface IGetUserByIdUsecase {
-  execute(userType: string, userId: string): Promise<IVendorEntity | IClientEntity>;
+  execute(userType: string, userId: string): Promise<VendorProfileDto | IClientEntity>;
 }
