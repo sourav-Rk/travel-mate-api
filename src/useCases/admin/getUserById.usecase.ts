c46420f83289090022c1d240d2a17d51ede55eb6
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 18:38:07 +0530

    added mapper

--- src/useCases/admin/getUserById.usecase.ts
+++ src/useCases/admin/getUserById.usecase.ts
@@ -1,11 +1,12 @@
 import { inject, injectable } from "tsyringe";
 
 import { IClientEntity } from "../../entities/modelsEntity/client.entity";
-import { IVendorEntity } from "../../entities/modelsEntity/vendor.entity";
 import { IClientRepository } from "../../entities/repositoryInterfaces/client/client.repository.interface";
 import { IVendorRepository } from "../../entities/repositoryInterfaces/vendor/vendor-repository.interface";
 import { IGetUserByIdUsecase } from "../../entities/useCaseInterfaces/admin/getUserById-usecase.interface";
+import { VendorMapper } from "../../interfaceAdapters/mappers/vendor.mapper";
 import { HTTP_STATUS } from "../../shared/constants";
+import { VendorProfileDto } from "../../shared/dto/vendor.dto";
 import { CustomError } from "../../shared/utils/error/customError";
 import { NotFoundError } from "../../shared/utils/error/notFoundError";
 
@@ -22,20 +23,24 @@ export class GetUserByIdUsecase implements IGetUserByIdUsecase {
   async execute(
     userType: string,
     userId: string
-  ): Promise<IVendorEntity | IClientEntity> {
+  ): Promise<VendorProfileDto | IClientEntity> {
     if (!userId || !userType)
       throw new CustomError(
         HTTP_STATUS.BAD_REQUEST,
         "user type and id are required"
       );
     if (userType === "vendor") {
-      const vendor = await this._vendorRepository.getVendorWithAddressAndKyc(userId);
+      const vendor = await this._vendorRepository.getVendorWithAddressAndKyc(
+        userId
+      );
       if (!vendor) {
         throw new NotFoundError("User not found");
       }
-      return vendor;
+
+      const vendorDetails = VendorMapper.mapVendorToFullInfoDto(vendor);
+      return vendorDetails;
     }
-    
+
     if (userType === "client") {
       const user = await this._clientRepository.findById(userId);
       if (!user) {
