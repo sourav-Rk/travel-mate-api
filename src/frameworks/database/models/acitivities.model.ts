ce168504ea3c98ea3068af49bf2bf6fa58dad3ea
SOURAV-RK <souravrk25@gmail.com>
2025-08-26 20:11:26 +0530

    omit the _id and extended it with object id

--- src/frameworks/database/models/acitivities.model.ts
+++ src/frameworks/database/models/acitivities.model.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+
 import { IActivitiesEntity } from "../../../entities/modelsEntity/activites.entity";
 import { activitiesSchema } from "../schemas/activities.schema";
 
-export interface IActivitiesModel extends IActivitiesEntity {};
+export interface IActivitiesModel extends Omit<IActivitiesEntity,"_id">,Document {
+    _id : mongoose.Types.ObjectId
+};
 export const activitiesDB =  mongoose.model("activities",activitiesSchema)
