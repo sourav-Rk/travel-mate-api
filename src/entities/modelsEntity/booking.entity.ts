f4d52b255468c37c5a69a00d18882b7442c05513
SOURAV-RK <souravrk25@gmail.com>
2025-09-02 20:36:57 +0530

    added booking entity

---
+++ src/entities/modelsEntity/booking.entity.ts
@@ -0,0 +1,23 @@
+import { BOOKINGSTATUS } from "../../shared/constants";
+
+export interface IBookingEntity {
+  _id?: string;
+  userId: string;
+  packageId: string;
+  status: BOOKINGSTATUS;
+  advancePayment?: {
+    amount: number;
+    paid: boolean;
+    dueDate: Date;
+    paidAt: Date | null;
+  };
+  fullPayment?: {
+    amount: number;
+    paid: boolean;
+    dueDate: Date;
+    paidAt: Date | null;
+  };
+  isWaitlisted?: boolean;
+  appliedAt?: Date;
+  cancelledAt?: Date;
+}
