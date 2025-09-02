703e9708e73685a39e26e5093af832044131cc45
SOURAV-RK <souravrk25@gmail.com>
2025-09-02 17:40:22 +0530

    changed the return type

--- src/entities/useCaseInterfaces/package/client-package/getAvailable-package-usecase.interface.ts
+++ src/entities/useCaseInterfaces/package/client-package/getAvailable-package-usecase.interface.ts
@@ -1,13 +1,14 @@
+import { PaginatedPackagesUserSideListing } from "../../../../shared/dto/packageDto";
 import { PaginatedPackages } from "../../../modelsEntity/paginated-packages.entity";
 
 export interface IGetAvailablePackagesUsecase {
   execute(
-    search: string,
+  search: string,
     categories: string[],
     priceRange: number[],
     duration: string,
     pageNumber: number,
     pageSize: number,
-    sortBy: string ,
-  ): Promise<PaginatedPackages>;
+    sortBy: string
+  ): Promise<PaginatedPackagesUserSideListing>;
 }
