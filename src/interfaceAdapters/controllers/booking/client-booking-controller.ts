9e81d010baa5b53427e0a254611fd4fa1dfe46f6
SOURAV-RK <souravrk25@gmail.com>
2025-09-02 21:38:47 +0530

    added booking,notification and fcm controller

---
+++ src/interfaceAdapters/controllers/booking/client-booking-controller.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from "tsyringe";
+import { IClientBookingController } from "../../../entities/controllerInterfaces/booking/client-booking-controller.interface";
+import { IApplyPackageUsecase } from "../../../entities/useCaseInterfaces/booking/client-booking/apply-package-usecase.interface";
+import { Request, Response } from "express";
+import { CustomRequest } from "../../middlewares/auth.middleware";
+import { BOOKINGSTATUS, HTTP_STATUS } from "../../../shared/constants";
+import { IGetBookingsUsecase } from "../../../entities/useCaseInterfaces/booking/client-booking/getBookings-usecase.interface";
+import { IGetBookingDetailsClientUsecase } from "../../../entities/useCaseInterfaces/booking/client-booking/get-booking-details-user-usecase.interface";
+
+@injectable()
+export class ClientBookingController implements IClientBookingController {
+  constructor(
+    @inject("IApplyPackageUsecase")
+    private _applyPackageUsecase: IApplyPackageUsecase,
+
+    @inject("IGetBookingDetailsUsecase")
+    private _getBookingDetailsUsecase: IGetBookingDetailsClientUsecase,
+
+    @inject("IGetBookingsUsecase")
+    private _getBookingsUsecase: IGetBookingsUsecase
+  ) {}
+
+  async applyPackage(req: Request, res: Response): Promise<void> {
+    const userId = (req as CustomRequest).user.id;
+    const { packageId } = req.body;
+    const response = await this._applyPackageUsecase.execute(userId, packageId);
+    res.status(response.statusCode).json(response.content);
+  }
+
+  async getBookingDetailOfPackage(req: Request, res: Response): Promise<void> {
+    const userId = (req as CustomRequest).user.id;
+    const { packageId } = req.params;
+    const response = await this._getBookingDetailsUsecase.execute(
+      userId,
+      packageId
+    );
+    res
+      .status(HTTP_STATUS.OK)
+      .json({ success: true, bookingDetails: response });
+  }
+
+  async getBookings(req: Request, res: Response): Promise<void> {
+    const userId = (req as CustomRequest).user.id;
+    const statuses = req.query.status
+      ? (req.query.status as string).split(",")
+      : [];
+    const response = await this._getBookingsUsecase.execute(
+      userId,
+      statuses as BOOKINGSTATUS[]
+    );
+    res.status(HTTP_STATUS.OK).json({ success: true, bookings: response });
+  }
+}
