14b0843405d1795b9549b77b42558ee412df6c4a
SOURAV-RK <souravrk25@gmail.com>
2025-09-18 12:54:36 +0530

    added wishlist mapper

---
+++ src/interfaceAdapters/mappers/wishlist.mapper.ts
@@ -0,0 +1,40 @@
+import { IWishlistEntity } from "../../entities/modelsEntity/wishlist.entity";
+import { IWishlistModel } from "../../frameworks/database/models/wishlist.model";
+import {
+  PackageDetailsWishlistDto,
+  WishlistDto,
+} from "../../shared/dto/wishlistDto";
+
+export class WishListMapper {
+  static toEntity(doc: IWishlistModel): IWishlistEntity {
+    return {
+      _id: String(doc._id),
+      userId: String(doc.userId),
+      packages: doc.packages.map((d) => String(d)),
+    };
+  }
+
+  static mapToWishlistListing(doc: any): WishlistDto {
+  return {
+    userId: String(doc.userId),
+    packages: doc.packagesDetails.map((pkg: any) => ({
+      _id: String(pkg._id),
+      packageName: pkg.packageName,
+      title: pkg.title,
+      category: pkg.category,
+      tags: pkg.tags,
+      images: pkg.images,
+      price: pkg.price,
+      duration: {
+        days: pkg.duration.days,
+        nights: pkg.duration.nights,
+      },
+      applicationDeadline: pkg.applicationDeadline,
+      status: pkg.status,
+      startDate: pkg.startDate,
+      endDate: pkg.endDate,
+    })),
+  };
+}
+
+}
