f9d228e4859dae413254076d2fd9e172b2d3f7bd
SOURAV-RK <souravrk25@gmail.com>
2025-09-18 12:09:16 +0530

    removed packageId as objectId

--- src/frameworks/database/models/booking.model.ts
+++ src/frameworks/database/models/booking.model.ts
@@ -2,9 +2,8 @@ import mongoose, { Document } from "mongoose";
 import { IBookingEntity } from "../../../entities/modelsEntity/booking.entity";
 import { bookingSchema } from "../schemas/booking.schema";
 
-export interface IBookingModel extends Omit<IBookingEntity,"_id" | "packageId" | "userId">,Document{
+export interface IBookingModel extends Omit<IBookingEntity,"_id" | "userId">,Document{
     userId : mongoose.Types.ObjectId,
-    packageId : mongoose.Types.ObjectId
 }
 
 export const bookingDB = mongoose.model("bookings",bookingSchema)
