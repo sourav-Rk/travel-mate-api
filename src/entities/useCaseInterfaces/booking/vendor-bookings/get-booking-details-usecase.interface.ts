0e6c2910be727cf3e62f66b5e68635ca091f8935
SOURAV-RK <souravrk25@gmail.com>
2025-09-18 12:07:56 +0530

    replaced with BookingDetailsWithUserDetailsDto

--- src/entities/useCaseInterfaces/booking/vendor-bookings/get-booking-details-usecase.interface.ts
+++ src/entities/useCaseInterfaces/booking/vendor-bookings/get-booking-details-usecase.interface.ts
@@ -1,6 +1,6 @@
-import { BookingDetailsDto, ClientPackageBookingDto } from "../../../../shared/dto/bookingDto";
+import { BookingDetailsWithUserDetailsDto } from "../../../../shared/dto/bookingDto";
 
 
 export interface IGetBookingDetailsVendorUsecase{
-    execute(bookingId : string) : Promise<BookingDetailsDto | null>;
+    execute(bookingId : string) : Promise<BookingDetailsWithUserDetailsDto | null>;
 }
