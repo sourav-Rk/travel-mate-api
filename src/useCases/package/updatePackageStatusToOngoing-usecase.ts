29dd99a19eca0d07215dc26904d696857a263f4a
SOURAV-RK <souravrk25@gmail.com>
2025-09-18 12:33:29 +0530

    added the condition to check the status is application_closed

--- src/useCases/package/updatePackageStatusToOngoing-usecase.ts
+++ src/useCases/package/updatePackageStatusToOngoing-usecase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { IUpdatePackageStatusToOngoingUsecase } from "../../entities/useCaseInterfaces/package/updatePackageStausToOngoing-usecase";
 import { IPackageRepository } from "../../entities/repositoryInterfaces/package/package-repository.interface";
+import { BOOKINGSTATUS } from "../../shared/constants";
 
 @injectable()
 export class UpdatePackageStatusToOngoing
@@ -12,15 +13,17 @@ export class UpdatePackageStatusToOngoing
   ) {}
 
   async execute(): Promise<void> {
-     const today = new Date();
+    const today = new Date();
 
-    const todayUTC = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate()));
+    const todayUTC = new Date(
+      Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate())
+    );
 
     const packageStartingToday =
       await this._packageRepository.findByPackagesByTodayDate(todayUTC);
 
     for (const pkg of packageStartingToday) {
-      if (pkg.status !== "ongoing" && pkg.status === "active") {
+      if (pkg.status !== "ongoing" && pkg.status === "applications_closed") {
         pkg.status = "ongoing";
         await this._packageRepository.updatePackageStatus(pkg?._id, "ongoing");
         console.log(`package ${pkg.packageName} marked as ongoing`);
