57472ce649c295625d324f6a48eaba8e354239a3
SOURAV-RK <souravrk25@gmail.com>
2025-09-18 12:55:47 +0530

    added addToWishlist,removeFromWishlist and getWishlist usecases

---
+++ src/useCases/wishlist/getWishlist.usecase.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from "tsyringe";
+import { IGetWishlistUsecase } from "../../entities/useCaseInterfaces/wishlist/getWishlist-usecase.interface";
+import { IWishListRepository } from "../../entities/repositoryInterfaces/wishlist/wishlist-repository.interface";
+import { WishlistDto } from "../../shared/dto/wishlistDto";
+import { IClientRepository } from "../../entities/repositoryInterfaces/client/client.repository.interface";
+import { ValidationError } from "../../shared/utils/error/validationError";
+import { ERROR_MESSAGE } from "../../shared/constants";
+import { NotFoundError } from "../../shared/utils/error/notFoundError";
+@injectable()
+export class GetWishlistUsecase implements IGetWishlistUsecase {
+  constructor(
+    @inject("IWishListRepository")
+    private _wishlistRepository: IWishListRepository,
+
+    @inject("IClientRepository")
+    private _clientRepository: IClientRepository
+  ) {}
+
+  async execute(userId: string): Promise<WishlistDto> {
+    if (!userId) {
+      throw new ValidationError(ERROR_MESSAGE.ID_REQUIRED);
+    }
+
+    const user = await this._clientRepository.findById(userId);
+    if (!user) {
+      throw new NotFoundError(ERROR_MESSAGE.USER_NOT_FOUND);
+    }
+
+    const wishlist =
+      await this._wishlistRepository.getWishlistWithPackageDetails(userId);
+
+    if (!wishlist) {
+      throw new NotFoundError(ERROR_MESSAGE.WISHLIST_NOT_FOUND);
+    }
+
+    return wishlist;
+  }
+}
