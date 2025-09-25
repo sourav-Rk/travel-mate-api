572f5bc174b97035d9b90718b1f3768664e74cbe
SOURAV-RK <souravrk25@gmail.com>
2025-09-25 06:28:55 +0530

    added findByGuideAhdUserId and findByPackageIdAndGuideId functions

--- src/entities/repositoryInterfaces/review/review-repository.interface.ts
+++ src/entities/repositoryInterfaces/review/review-repository.interface.ts
@@ -5,4 +5,6 @@ import { IBaseRepository } from "../baseRepository.interface";
 export interface IReviewRepository extends IBaseRepository<IReviewEntity>{
     findByPackageIdAndUserId(userId : string,packageId : string) : Promise<IReviewEntity | null>;
     findByPackageId(packageId : string) : Promise<ReviewListWithUserDetailsDto[] | null>;
+    findByGuideIdAndUserId(userId : string,guideId : string) : Promise<IReviewEntity | null>;
+    findByPackageIdAndGuideId(packageId : string,guideId : string) : Promise<ReviewListWithUserDetailsDto[] | null>;
 }
